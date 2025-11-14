678f47f7f0daa5131cdf14966ab5cd3b414b1bf2
zainashraf1562 <zainashraf1562@gmail.com>
2025-11-13 21:28:55 -0500

    finished cart class

--- src/main/java/com/pluralsight/waffleShop/OrderData/Cart.java
+++ src/main/java/com/pluralsight/waffleShop/OrderData/Cart.java
@@ -1,4 +1,32 @@
 package com.pluralsight.waffleShop.OrderData;
 
+import com.pluralsight.waffleShop.Products.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<Product> myCart;
+
+    public Cart(){
+        myCart = new ArrayList<>();
+    }
+
+    public List<Product> getProduct() {
+        return myCart;
+    }
+
+    public void addProduct(Product product){
+        myCart.add(product);
+    }
+
+    public double getTotalCost(){
+        double sum = 0;
+
+        for (Product product : myCart) {
+            sum += product.getPrice();
+        }
+        return sum;
+    }
+
 }
